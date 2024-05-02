14cfcb2729c7a74bd117becef1d6fdab19c037ec
Lasha Gogolashvili <lashagogolashvili17@gmail.com>
2024-05-03 02:41:55 +0400

    applied styles on fetched data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { MainLayout } from "./common/components/_organisms";
 import useFetch from "./common/hooks/useFetch";
 import ratingsvg from "../public/Rating.svg";
-import cartbtn from "../public/cart-btn.png"
+import cartbtn from "../public/cart-btn.png";
 
 const App = () => {
   const { data, loading, error } = useFetch(
@@ -18,25 +18,35 @@ const App = () => {
   }
   return (
     <MainLayout>
-      <div>
-        {data?.map((product: any) => (
-          <div key={product.id}>
-            <img
-              src={product.image}
-              alt={product.title}
-              width={150}
-              height={150}
-            />
-            <div>
-              <div className="w-[321px] grid-cols-2  place-content-center">
-                <h4>{product.title}</h4>
-                <span>${product.price}</span>
-                <img src={ratingsvg} alt="rating" />
+      <div className="flex justify-center">
+        <div className="grid place-content-around grid-cols-3 justify-items-center gap-[15px] pt-20">
+          {data?.map((product: any) => (
+            <div
+              key={product.id}
+              className="w-[200px] h-[400px] flex justify-evenly flex-col border border-graywhite rounded-lg px-4 cursor-pointer hover:border-hovergreen transition duration-300 ease-in-out hover:shadow-lg hover:bg-green-500 hover:bg-opacity-10"
+            >
+              <img
+                src={product.image}
+                alt={product.title}
+                width={300}
+                height={300}
+                className="h-[200px] w-[200px]"
+              />
+              <div className="flex">
+                <div>
+                  <h4 className="text-[14px]">{product.title}</h4>
+                  <span>${product.price}</span>
+                  <img src={ratingsvg} alt="rating" />
+                </div>
+                <button className="items-end flex w-[50px]">
+                  <div className="bg-graywhite rounded-xl w-[25px] h-[25px] flex items-center justify-center">
+                    <img src={cartbtn} alt="cart icon" width={15} height={15} />
+                  </div>
+                </button>
               </div>
-              <button><img src={cartbtn} alt="cart icon" width={15} height={15}/></button>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </MainLayout>
   );
