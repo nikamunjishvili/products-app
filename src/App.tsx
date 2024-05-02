241abcd6435c5a4a8e3a3d49ce4e9da7a8ae630b
Ivane <gdzelishviliv@gmail.com>
2024-05-03 00:47:12 +0400

    fetched product data using custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
 import { MainLayout } from "./common/components/_organisms";
+import useFetch from "./common/hooks/useFetch";
+import ratingsvg from "../public/Rating.svg";
+import cartbtn from "../public/cart-btn.png"
 
 const App = () => {
+  const { data, loading, error } = useFetch(
+    "https://fakestoreapi.com/products"
+  );
+  if (loading) {
+    return <MainLayout>Loading...</MainLayout>;
+  }
+  if (error) {
+    return <MainLayout>Error:{error}</MainLayout>;
+  }
+  if (!data || !Array.isArray(data)) {
+    return <MainLayout>No data available</MainLayout>;
+  }
   return (
     <MainLayout>
-      <h1>App</h1>
+      <div>
+        {data?.map((product: any) => (
+          <div key={product.id}>
+            <img
+              src={product.image}
+              alt={product.title}
+              width={150}
+              height={150}
+            />
+            <div>
+              <div>
+                <h4>{product.title}</h4>
+                <span>${product.price}</span>
+                <img src={ratingsvg} alt="rating" />
+              </div>
+              <button><img src={cartbtn} alt="cart icon" width={15} height={15}/></button>
+            </div>
+          </div>
+        ))}
+      </div>
     </MainLayout>
   );
 };
