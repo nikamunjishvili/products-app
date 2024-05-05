ab999797f7dbc7d760292e8c882875551042af87
Norzalovi1 <jemo.norzalovi@gmail.com>
2024-05-05 22:07:19 +0400

    styled secondpage

--- src/pages/productpage/ProductPage.tsx
+++ src/pages/productpage/ProductPage.tsx
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom";
 import { MainLayout } from "../../common/components/_organisms";
 import useFetch from "../../common/hooks/useFetch";
+import Love from "../../common/components/_atoms/Icon/Love";
+import Shop from "../../common/components/_atoms/Icon/Shop";
+
 
 const ProductPage = () => {
   const { id } = useParams();
@@ -17,20 +20,24 @@ const ProductPage = () => {
     <MainLayout>
       {data && (
         <>
-          <div>
-            <div>
-              <img src={data.image} alt={data.title} width={100} height={100} />
+          <div className=" w-[1320px] h-[558px] mx-auto flex" >
+            <div className="w-[700px]">
+               <img className="w-[300px] h-[350px] relative left-[144px] top-[54px]" src={data.image} alt={data.title} width={100} height={100}  />
             </div>
-            <div>
-              <h1>{data.title}</h1>
-              <p>{data.price}</p>
-              <p>{data.description}</p>
-              <div className="flex gap-5">
-                <button className="bg-carthovergreen">add to cart</button>
-                <button>heart button</button>
+            <div className="relative ml-8 w-[1029px]">
+              <h1 className="absolute top-[71px] right-[273px] w-[493px] text-xl font-bold 0">
+                {data.title}
+              </h1>
+              <p className="relative  top-[139px] text-bordergreen font-bold text-2xl"> 
+                ${data.price}
+              </p>
+              <p className="relative  top-[167px] text-lightgray">{data.description}</p>
+              <div className="relative  top-[213px] flex gap-5 ">
+              <button className="bg-carthovergreen py-2 px-4 rounded-3xl w-[350px] flex flex-row-reverse items-center justify-center gap-4"><Shop  width={15} height={15}/> Add to Cart</button>
+                <button ><Love  width={52} height={52}/></button>
               </div>
-              <p>
-                category: <span>{data.category}</span>
+              <p className="relative  top-[291px] mt-2 text-lightgray ">
+                Category: <span>{data.category}</span>
               </p>
             </div>
           </div>
