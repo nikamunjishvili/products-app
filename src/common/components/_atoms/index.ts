8451bdc0aff6a5a9eb4f0f44a5c0326e98a0ec0f
Ivane <gdzelishviliv@gmail.com>
2024-05-10 02:16:44 +0400

    optimized import and fixed some UI issues

--- src/common/components/_atoms/index.ts
+++ src/common/components/_atoms/index.ts
@@ -1,2 +1,7 @@
 export {default as HomeIcon} from './Icon/Home';
 export {default as Rightarrow} from './Icon/Right-arrow'
+export {default as Heart} from './Icon/Heart'
+export {default as Home} from './Icon/Home'
+export {default as Love} from './Icon/Love'
+export {default as Plant} from './Icon/Plant'
+export {default as Shop} from './Icon/Shop'
--- src/common/components/_molecules/Navbar/navbar.tsx
+++ src/common/components/_molecules/Navbar/navbar.tsx
@@ -1,22 +1,31 @@
-import Plant from "../Public/plant.svg"
-import Heart from "../Public/Heart.svg"
+import { Link } from "react-router-dom";
+import { Heart, Plant } from "../../_atoms";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[80px] bg-white text-black flex  items-center">
-     <img className="ml-[200px]" src={Plant} alt="" />
-     <h1 className="text-4xl font-semibold ml-[5px]	">Ecobazar</h1>
-     <div className="flex ml-[200px]">
-     <input className="w-[300px]  h-[40px] rounded outline-none" placeholder="Search" type="text" />
-     <button className="bg-green-500 w-[120px] h-[40px] rounded-r-lg text-white"  >Search</button>
-     </div>
-     <div className="w-[400px] h-[50px] ml-[50px] flex items-center">
-     <img className="ml-[160px]" src={Heart} alt="" />
-     <h6 className="w-[600px] ml-[20px]">Shopping Cart:<span>57.00$</span></h6>
-     </div>
+    <div className="w-full h-[80px] bg-white text-black flex  items-center justify-between px-72">
+      <Link to={"/"} className="flex">
+        <Plant width={50} height={50} />
+        <h1 className="text-4xl font-semibold ml-[5px]	">Ecobazar</h1>
+      </Link>
+      <div className="flex">
+        <input
+          className="w-[300px]  h-[40px] rounded outline-none border-y border-l border-graywhite pl-5"
+          placeholder="Search"
+          type="text"
+        />
+        <button className="bg-lightgreen w-[120px] h-[40px] rounded-r-lg text-white text-base font-semibold	">
+          Search
+        </button>
+      </div>
+      <div className="flex items-center">
+        <Heart width={45.67} height={27.8} />
+        <h6>
+          Shopping Cart:<span>57.00$</span>
+        </h6>
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
