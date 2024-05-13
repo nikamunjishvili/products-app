a3c94aaa8eb8055aca522877952344018a004b0f
Nika munjisvili <nikamunja31415@gmail.com>
2024-05-13 16:14:44 +0400

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,16 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import ProductPage from "./pages/productpage/ProductPage.tsx";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-    },
-    {
-        path: "/:id",
-        element: <ProductPage />
-    }
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/:id",
+    element: <ProductPage />,
+  },
 ]);
 
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <RouterProvider router={router} />
-  );
+  <RouterProvider router={router} />
+);
